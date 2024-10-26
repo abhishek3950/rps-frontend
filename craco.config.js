c3762b221cff3aed533e01bee6a24862192d29c3
Abhishek Bansal <abhishekthaparian@gmail.com>
2024-10-27 01:26:00 +0700

    Add assert polyfill and update CRACO configuration

--- craco.config.js
+++ craco.config.js
@@ -10,12 +10,17 @@ module.exports = {
         util: require.resolve('util/'),
         buffer: require.resolve('buffer/'),
         process: require.resolve('process/browser'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
       };
       webpackConfig.plugins = [
         ...webpackConfig.plugins,
         new webpack.ProvidePlugin({
           Buffer: ['buffer', 'Buffer'],
           process: 'process/browser',
+          assert: 'assert', // Add this line
+          crypto: 'crypto-browserify', // Added
+          stream: 'stream-browserify', // Added
         }),
       ];
       return webpackConfig;
